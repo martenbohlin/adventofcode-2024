679d06e0645bb0de7138c645e3b2ccb539613fda
Marten Bohlin <marten.bohlin@avinode.com>
2024-12-02 17:30:39 +0100

    Dec 2 part 2

--- dec2/src/main.rs
+++ dec2/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     // Part 1
     let mut safe = 0;
-    'outer: for line in lines {
+    'outer: for line in lines.clone() {
         let mut last_level = -1;
         let mut last_direction = 0;
         for level in line.split_whitespace() {
@@ -30,6 +30,57 @@ fn main() {
         safe += 1;
     }
     println!("Part 1: {}", safe);
+
+    // Part 2
+    let mut safe = 0;
+    for line in lines.clone() {
+        let levels = line.split_whitespace()
+            .map(|level:&str| level.parse().unwrap())
+            .collect::<Vec<i32>>();
+        let skipped_levels = bad_levels(&levels); // Handles failed once it got started
+        if skipped_levels < 2 {
+            safe += 1;
+        } else {
+            if bad_levels(&levels[1..].to_vec()) == 0  { // Was the first one bad?
+                safe += 1;
+            } else {
+                let mut a = levels[0..1].to_vec();
+                let b = levels[2..].to_vec();
+                a.extend(b);
+                if bad_levels(&a) == 0 { // Was the second one bad?
+                    safe += 1;
+                }
+            }
+        }
+    }
+    println!("Part 2: {}", safe);
+
+}
+
+fn bad_levels(levels: &Vec<i32>) -> i32 {
+    let mut last_level = -1;
+    let mut last_direction = 0;
+    let mut skipped_levels = 0;
+    for level in levels {
+        if last_level == -1 {
+            last_level = *level;
+        } else {
+            let diff = level - last_level;
+            if diff == 0 || diff.abs() > 3 {
+                skipped_levels += 1;
+                continue;
+            }
+            let direction = if diff > 0 { 1 } else { -1 };
+            let direction_diff: i32 = direction - last_direction;
+            if direction_diff.abs() > 1 {
+                skipped_levels += 1;
+                continue;
+            }
+            last_direction = direction;
+            last_level = *level;
+        }
+    }
+    skipped_levels
 }
 
 fn read_lines(filename: String) -> Vec<String> {
