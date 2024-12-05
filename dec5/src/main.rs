278fc9cc5e704fcb191ae0f64aab2ca2fafc507e
Marten Bohlin <marten.bohlin@avinode.com>
2024-12-05 13:51:20 +0100

    Dec 5 part 1

---
+++ dec5/src/main.rs
@@ -0,0 +1,71 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::env;
+use std::fs::read_to_string;
+
+fn main() {
+    let (rules, updates) = read_file(env::args().collect::<Vec<String>>()[1].clone());
+    part1(rules, updates);
+}
+
+fn part1(rules: HashMap<i32,HashSet<i32>>, updates: Vec<Vec<i32>>) {
+    let mut sum = 0;
+    for update in updates {
+        if update_ok(&rules, &update) {
+            sum += update[(update.len()-1)/2];
+        }
+    }
+    println!("Part 1: {}", sum);
+}
+
+fn update_ok(rules: &HashMap<i32,HashSet<i32>>, update: &Vec<i32>) -> bool {
+    for i in 1..update.len() {
+        match  rules.get(&update[i]){
+            Some(rule) => {
+                for j in 0..i {
+                    if rule.contains(&update[j]) {
+                        return false;
+                    }
+                }
+            },
+            None => continue,
+        }
+    }
+    return true;
+}
+
+fn read_file(filename: String) -> (HashMap<i32,HashSet<i32>>, Vec<Vec<i32>>) {
+    let all_lines:Vec<String> = read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(|line| line.chars().collect())  // make each slice into a string
+        .collect();  // gather them together into a vector
+
+    let mut rules = HashMap::new();
+    let mut updates = Vec::new();
+    let mut rules_done = false;
+    for line in all_lines {
+        if line.len() == 0 {
+            rules_done = true;
+        } else if !rules_done {
+            let (first, later) = parse_rule(&line);
+            rules.entry(first).or_insert(HashSet::new()).insert(later);
+        } else {
+            updates.push(parse_update(&line));
+        }
+
+    }
+
+    return (rules, updates);
+}
+
+fn parse_rule(line: &String) -> (i32, i32) {
+    let parts = line.split("|").collect::<Vec<&str>>();
+    (parts[0].parse::<i32>().unwrap(), parts[1].parse::<i32>().unwrap())
+}
+
+fn parse_update(line: &String) -> Vec<i32> {
+    line.split(",")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+}
