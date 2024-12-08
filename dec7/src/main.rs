31a1498bf28f295aa049017a0b7d70b2b267d9ba
Marten Bohlin <marten.bohlin@avinode.com>
2024-12-08 12:07:16 +0100

    Dec 7 part 2

--- dec7/src/main.rs
+++ dec7/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         .collect();
 
     part1(&file);
+    part2(&file);
 }
 
 fn part1(lines: &Vec<String>) {
@@ -23,6 +24,18 @@ fn part1(lines: &Vec<String>) {
     println!("Part 1: {}", sum);
 }
 
+fn part2(lines: &Vec<String>) {
+    let mut sum = 0;
+    for line in lines {
+        let(result, numbers) = parse_line(line);
+        if test_p2(result, numbers[0], &numbers[1..].to_vec()) {
+            sum += result;
+        }
+    }
+
+    println!("Part 2: {}", sum);
+}
+
 fn test(result: i64, now: i64, numbers: &Vec<i64>) -> bool {
     return match numbers[..] {
         [] => now == result, // Did we get the correct result?
@@ -31,6 +44,26 @@ fn test(result: i64, now: i64, numbers: &Vec<i64>) -> bool {
                 test(result, now * numbers[0], &numbers[1..].to_vec()) // Will * give correct result?
         }
     };
+}
+
+fn test_p2(result: i64, now: i64, numbers: &Vec<i64>) -> bool {
+    if now > result {
+        return false;
+    }
+    return match numbers[..] {
+        [] => now == result, // Did we get the correct result?
+        _ => {
+            test_p2(result, now + numbers[0], &numbers[1..].to_vec()) || // Will + give the correct result?
+                test_p2(result, now * numbers[0], &numbers[1..].to_vec()) ||// Will * give correct result?
+                test_p2(result, concatenation(now, numbers[0]), &numbers[1..].to_vec())
+        }
+    };
+}
+
+fn concatenation(a: i64, b: i64) -> i64 {
+    let mut a_string = a.to_string();
+    a_string.push_str(&b.to_string());
+    return a_string.parse::<i64>().unwrap();
 }
 
 fn parse_line(line: &String) -> (i64, Vec<i64>) {
