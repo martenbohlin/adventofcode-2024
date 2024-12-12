88977178bffd3ad4fdfda1a62afcd26ffac775f0
Marten Bohlin <marten.bohlin@avinode.com>
2024-12-12 22:26:16 +0100

    Dec 12 part 2

--- dec12/src/main.rs
+++ dec12/src/main.rs
@@ -3,17 +3,29 @@ use std::env;
 
 fn main() {
     let mut map = read_file(env::args().collect::<Vec<String>>()[1].clone());
-    let areas = find_areas(&mut map);
+    let mut areas = find_areas(&mut map);
+    for area in &mut areas {
+        set_sides_by_count_corners(area, map[0].len(), map.len());
+    }
+
     part1(&areas);
+    part2(&areas);
 }
 
 fn part1(areas: &Vec<Area>) {
     let mut sum = 0;
     for area in areas {
-        let a = area;
-        sum += a.perimeter * a.area;
+        sum += area.perimeter * area.area;
     }
     println!("Part 1: {}", sum);
+}
+
+fn part2(areas: &Vec<Area>) {
+    let mut sum = 0;
+    for area in areas {
+        sum += area.sides * area.area;
+    }
+    println!("Part 2: {}", sum);
 }
 
 fn find_areas(map: &mut Vec<Vec<char>>) -> Vec<Area> {
@@ -22,7 +34,7 @@ fn find_areas(map: &mut Vec<Vec<char>>) -> Vec<Area> {
     for y in 0..map.len() {
         for x in 0..map[y].len() {
             if map[y][x] != '.' {
-                let mut area = Area { plant: map[y][x], plots: Vec::new(), area: 0, perimeter: 0 };
+                let mut area = Area { plant: map[y][x], plots: Vec::new(), area: 0, perimeter: 0, sides: 0 };
                 find_area(map, &mut area, &(x,y));
                 result.push(area);
             }
@@ -57,6 +69,7 @@ fn add_to_area(area: &mut Area, coordinate: &(usize, usize)) {
     area.plots.push(coordinate.clone());
     area.area += 1;
     area.perimeter += 4;
+    area.sides += 4;
     let x = coordinate.0;
     let y = coordinate.1;
     if area.plots.contains(&(x.wrapping_sub(1), y)) {
@@ -73,12 +86,35 @@ fn add_to_area(area: &mut Area, coordinate: &(usize, usize)) {
     }
 }
 
+fn set_sides_by_count_corners(area: &mut Area, width: usize, height: usize) {
+    let mut corners = 0;
+    for y in 0..=height {
+        for x in 0..=width {
+            // The corner grid is shifted up left compared to the map of plots
+            let up = ((x.wrapping_sub(1), y), (x, y.wrapping_sub(1)));
+            let down = ((x.wrapping_sub(1), y.wrapping_sub(1)), (x, y));
+            let up_has = (area.plots.contains(&up.0), area.plots.contains(&up.1));
+            let down_has = (area.plots.contains(&down.0), area.plots.contains(&down.1));
+
+            if up_has.0 == up_has.1 && up_has.0 == down_has.0  && up_has.0 == down_has.1 { // Not a corner if all are the same
+            } else if (up_has.0 != up_has.1) && (down_has.0 != down_has.1) { // Not a corner if both diagonals are different
+            } else if (up_has.0 != up_has.1) != (down_has.0 != down_has.1) { // 1 corner if one diagonal is different
+                corners += 1;
+            } else if (up_has.0 == up_has.1) && (down_has.0 == down_has.1) { // Two corners are meeting at this point
+                corners += 2;
+            }
+        }
+    }
+    area.sides = corners;
+}
+
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Area {
     plant: char,
     plots: Vec<(usize,usize)>,
     area: i32,
     perimeter: i32,
+    sides: i32,
 }
 
 
