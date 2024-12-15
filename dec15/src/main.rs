70699df393f08bbe4934d49a0e1abea3a9b030f6
Marten Bohlin <marten.bohlin@avinode.com>
2024-12-15 11:28:18 +0100

    Dec 15 part 2

--- dec15/src/main.rs
+++ dec15/src/main.rs
@@ -4,19 +4,20 @@ use std::fs::read_to_string;
 fn main() {
     let (map, commands) = read_file(env::args().collect::<Vec<String>>()[1].clone());
     let coordinate = find_robot(&map);
-    part1(coordinate, &map, commands);
+    part1(coordinate, &map, &commands);
+    part2(coordinate, &map, &commands);
 }
 
-fn part1(_coordinate: (usize, usize), _map: &Vec<Vec<char>>, commands: Vec<char>) {
+fn part1(_coordinate: (usize, usize), _map: &Vec<Vec<char>>, commands: &Vec<char>) {
     let mut map = _map.clone();
     let mut coordinate = _coordinate;
-    debug_print(&map);
-    println!("{:?} {:?}", coordinate, commands);
+    //debug_print(&map);
+    //println!("{:?} {:?}", coordinate, commands);
 
     for command in commands {
         coordinate = move_robot(&mut map, coordinate, command);
-        debug_print(&map);
-        println!("{:?} {:?}", coordinate, command);
+        //debug_print(&map);
+        //println!("{:?} {:?}", coordinate, command);
     }
 
     let mut sum = 0;
@@ -30,33 +31,104 @@ fn part1(_coordinate: (usize, usize), _map: &Vec<Vec<char>>, commands: Vec<char>
     println!("Part 1: {}", sum);
 }
 
-fn move_robot(map: &mut Vec<Vec<char>>, coordinate: (usize, usize), command: char) -> (usize, usize) {
+fn part2(_coordinate: (usize, usize), _map: &Vec<Vec<char>>, commands: &Vec<char>) {
+    let mut map = widen_map(_map);//_map.clone(); //
+    let mut coordinate = (_coordinate.0*2, _coordinate.1); //_coordinate.clone();//
+
+    for command in commands {
+        coordinate = move_robot(&mut map, coordinate, command);
+    }
+
+    let mut sum = 0;
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            if map[y][x] == '[' {
+                sum += 100 * y + x;
+            }
+        }
+    }
+    println!("Part 2: {}", sum);
+}
+
+fn widen_map(_map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut map = Vec::new();
+    for y in 0.._map.len() {
+        let mut row = Vec::new();
+        for x in 0.._map[y].len() {
+            if _map[y][x] == 'O' {
+                row.push('[');
+                row.push(']');
+            } else if _map[y][x] == '@' {
+                row.push('@');
+                row.push('.');
+            } else {
+                row.push(_map[y][x]);
+                row.push(_map[y][x]);
+            }
+        }
+        map.push(row);
+    }
+    return map;
+}
+
+fn move_robot(map: &mut Vec<Vec<char>>, coordinate: (usize, usize), command: &char) -> (usize, usize) {
     let new_coordinate = move_coordinate(&coordinate, &command);
-    if !move_box(map, new_coordinate, command) {
+    if !move_box(map, new_coordinate, command, true) {
         return coordinate;
     }
+    move_box(map, new_coordinate, command, false);
 
     map[coordinate.1][coordinate.0] = '.';
     map[new_coordinate.1][new_coordinate.0] = '@';
     return new_coordinate;
 }
 
-fn move_box(map: &mut Vec<Vec<char>>, coordinate: (usize, usize), command: char) -> bool {
+fn move_box(map: &mut Vec<Vec<char>>, coordinate: (usize, usize), command: &char, dry_run: bool) -> bool {
     return match map[coordinate.1][coordinate.0] {
         '#' => false,
         '.' => true,
         'O' => {
             let new_coordinate = move_coordinate(&coordinate, &command);
-            if move_box(map, new_coordinate, command) {
-                map[coordinate.1][coordinate.0] = '.';
-                map[new_coordinate.1][new_coordinate.0] = 'O';
+            if move_box(map, new_coordinate, command, dry_run) {
+                if !dry_run {
+                    map[coordinate.1][coordinate.0] = '.';
+                    map[new_coordinate.1][new_coordinate.0] = 'O';
+                }
                 true
             } else {
                 false
             }
         },
+        '[' => move_large_box(map, command, coordinate, dry_run),
+        ']' => move_large_box(map, command, (coordinate.0-1, coordinate.1), dry_run),
         _ => panic!("Unknown box: {} {:?}", map[coordinate.1][coordinate.0] , coordinate),
     };
+}
+
+fn move_large_box(map: &mut Vec<Vec<char>>, command: &char, coordinate:(usize,usize), dry_run: bool) -> bool {
+    let new_coordinate = move_coordinate(&coordinate, &command);
+    let (x1,y) = coordinate;
+    let x2 = x1 + 1;
+    let nx1 = new_coordinate.0;
+    let nx2 = nx1 + 1;
+    let ny = new_coordinate.1;
+
+    map[y][x1] = '.';
+    map[y][x2] = '.';
+    if move_box(map, (nx1, ny), command, dry_run) && move_box(map, (nx2, ny), command, dry_run) {
+        if dry_run {
+            map[y][x1] = '[';
+            map[y][x2] = ']';
+        } else {
+            map[ny][nx1] = '[';
+            map[ny][nx2] = ']';
+        }
+        true
+    } else {
+        map[y][x1] = '[';
+        map[y][x2] = ']';
+        false
+    }
 }
 
 fn move_coordinate(coordinate: &(usize, usize), command: &char) -> (usize, usize) {
